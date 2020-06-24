d7e7f4bf9215cd5a0d3c797a2ac564967a770893
Dariusz <dardomanski@gmail.com>
2020-06-24 16:33:59 +0200

    Updates

---
+++ precache-manifest.fdc090942ea4cb6772e5b3d58b2ac288.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6066e67e20a6c420bbf5f6193dba5da3",
+    "url": "/elbud/index.html"
+  },
+  {
+    "revision": "685f7a112cb3f256b266",
+    "url": "/elbud/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fc864693fba746d3ac0d",
+    "url": "/elbud/static/css/main.78f525f3.chunk.css"
+  },
+  {
+    "revision": "685f7a112cb3f256b266",
+    "url": "/elbud/static/js/2.6ccf7ee8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elbud/static/js/2.6ccf7ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc864693fba746d3ac0d",
+    "url": "/elbud/static/js/main.b5c65333.chunk.js"
+  },
+  {
+    "revision": "689c60043fe6a7e62b14",
+    "url": "/elbud/static/js/runtime-main.d9b0123e.js"
+  },
+  {
+    "revision": "eff626f63b886e81da9cf20708497e4f",
+    "url": "/elbud/static/media/logo.eff626f6.png"
+  }
+]);
